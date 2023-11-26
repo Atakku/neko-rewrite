4833f3d86c5f13b7ed942cabb4ccd55e4198fb71
Atakku <atakkudev@gmail.com>
2023-11-26 11:30:47 +0100

    Added wip axum module

---
+++ modules/axum/src/module.rs
@@ -0,0 +1,37 @@
+// Copyright 2023 Atakku <https://atakku.dev>
+//
+// This project is dual licensed under MIT and Apache.
+
+use axum::{Router, Server};
+use futures::future::BoxFuture;
+use neko_core::*;
+use std::net::Ipv4Addr;
+
+pub use axum;
+
+pub type Route = fn(Router) -> BoxFuture<'static, Res<Router>>;
+
+module! {
+  Axum {
+    routes: Vec<Route>,
+    port: u16 = get_env!("HTTP_PORT", 8080, u16),
+  }
+
+  impl on_runtime(axum) {
+    let mut router = Router::new();
+    for route in axum.routes {
+      router = route(router).await?;
+    }
+    future!({
+      Server::bind(&(Ipv4Addr::UNSPECIFIED, axum.port).into())
+        .serve(router.into_make_service())
+        .await?;
+      Ok(())
+    })
+  }
+
+  // TODO: resolve the issue with overlap
+  fn add_route(&mut self, route: Route) {
+    self.routes.push(route);
+  }
+}
